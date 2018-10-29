517a78d1144b9342ec93bde821a30a29abeee8cc
equation314 <equation618@gmail.com>
2018-10-29 13:25:29 +0800

    aarch64: implement mini UART for bcm2837

--- crate/bcm2837/src/asm.rs
+++ crate/bcm2837/src/asm.rs
@@ -2,9 +2,9 @@
 
 /// delay for some clocks
 #[inline]
-pub unsafe fn delay(clock: u32) {
+pub unsafe fn delay(_clock: u32) {
     #[cfg(target_arch = "aarch64")]
     asm!("mov x1, x0; 1: subs x1, x1, #1; bne 1b;"
-        :: "{x0}"(clock)
+        :: "{x0}"(_clock)
         :: "volatile");
 }
