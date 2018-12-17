f954c2fd6a0f78d3d0855c68e2f42f4393543dbc
dzy <daizy15@mails.tsinghua.edu.cn>
2018-12-17 16:45:04 +0800

    Completely get rid of cargo-generated target/

--- kernel/src/arch/riscv32/compiler_rt.rs
+++ kernel/src/arch/riscv32/compiler_rt.rs
@@ -2,23 +2,6 @@
 //!
 //! [atomic](http://llvm.org/docs/Atomics.html#libcalls-atomic)
 
-/// Copy from:
-/// https://github.com/rust-lang-nursery/compiler-builtins/blob/master/src/riscv32.rs
-#[no_mangle]
-pub extern fn __mulsi3(mut a: u32, mut b: u32) -> u32 {
-    let mut r: u32 = 0;
-
-    while a > 0 {
-        if a & 1 > 0 {
-            r += b;
-        }
-        a >>= 1;
-        b <<= 1;
-    }
-
-    r
-}
-
 #[no_mangle]
 pub extern fn abort() {
     loop {}
