613102287fa492376ee7cbdcb3b177e501365814
NagiNikaido <naginikaido@gmail.com>
2019-05-05 22:38:25 +0800

    struct emmc to be filled.

--- kernel/src/arch/aarch64/board/raspi3/emmc.rs
+++ kernel/src/arch/aarch64/board/raspi3/emmc.rs
@@ -2,8 +2,49 @@ use bcm2837::emmc::*;
 use core::time::Duration;
 use crate::thread;
 
-struct EmmcCtl {
+#[derive(Debug)]
+pub struct SDScr {
+    scr: [u32; 2],
+    sd_bus_widths:  u32,
+    sd_version: i32
+}
+
+impl SDScr {
+    pub fn new() -> SDScr {
+        SDScr {
+            scr: [0u32; 2],
+            sd_bus_widths: 0,
+            sd_version: 0
+        }
+    }
+}
+
+pub struct EmmcCtl {
     emmc: Emmc,
+    card_supports_sdhc: u32,
+    card_supports_18v:  u32,
+    card_ocr:           u32,
+    card_rca:           u32,
+    last_interrupt:     u32,
+    last_error:         u32,
+
+    sd_scr: SDScr,
+    failed_voltage_switch: i32,
+
+    last_cmd_reg:       u32,
+    last_cmd:           u32,
+    last_cmd_success:   u32,
+    last_r0:            u32,
+    last_r1:            u32,
+    last_r2:            u32,
+    last_r3:            u32,
+
+    // void *buf;
+    // int blocks_to_transfer;
+    block_size:         usize,
+    use_sdma:           i32,
+    card_removal:       i32,
+    base_clock:         u32
 }
 
 fn usleep(cnt: u32) {
@@ -26,16 +67,41 @@ fn usleep(cnt: u32) {
  * static void sd_handle_interrupts(struct emmc_block_dev *dev)
  * static void sd_issue_command(struct emmc_block_dev *dev, uint32_t command, uint32_t argument, useconds_t timeout)
  * static int sd_ensure_data_mode(struct emmc_block_dev *edev)
- * static int sd_suitable_for_dma(void *buf)
+ * -- static int sd_suitable_for_dma(void *buf)
  * static int sd_do_data_command(struct emmc_block_dev *edev, int is_write, uint8_t *buf, size_t buf_size, uint32_t block_no)
+ * int sd_card_init(struct block_device **dev)
+ * int sd_read(struct block_device *dev, uint8_t *buf, size_t buf_size, uint32_t block_no)
+ * int sd_write(struct block_device *dev, uint8_t *buf, size_t buf_size, uint32_t block_no)
  * Other Constants
  */
 
 impl EmmcCtl {
 
-    pub fn new() -> EmmcCtl {
+    pub fn new() -> EmmcCtl { //TODO: improve it!
         EmmcCtl {
             emmc: Emmc::new(),
+            card_supports_sdhc:0,
+            card_supports_18v:0,
+            card_ocr:0,
+            card_rca:0,
+            last_interrupt:0,
+            last_error:0,
+
+            sd_scr: SDScr::new(),
+            failed_voltage_switch:0,
+
+            last_cmd_reg:0,
+            last_cmd:0,
+            last_cmd_success:0,
+            last_r0:0,
+            last_r1:0,
+            last_r2:0,
+            last_r3:0,
+
+            block_size:0,
+            use_sdma:0,
+            card_removal:0,
+            base_clock:0
         }        
     }
 
@@ -108,5 +174,48 @@ impl EmmcCtl {
 
     pub fn sd_reset_cmd(&mut self) -> bool {
         false
+    }
+
+    pub fn sd_reset_dat(&mut self) -> bool {
+        false
+    }
+
+    pub fn sd_issue_command_int(&mut self, cmd_reg: u32, argument: u32, timeout: u32) {
+
+    }
+
+    pub fn sd_handle_card_interrupt(&mut self) {
+
+    }
+
+    pub fn sd_handle_interrupts(&mut self) {
+
+    }
+
+    pub fn sd_issue_command(&mut self, command: u32, argument: u32, timeout: u32) {
+
+    }
+
+    pub fn sd_ensure_data_mode(&mut self) -> i32 {
+        0
+    }
+
+    // sdma not implemented.
+    // pub fn sd_suitable_for_dma()
+
+    pub fn sd_do_data_command(&mut self, is_write: bool, buf: &[u8], block_no: u32) -> i32 {
+        0
+    }
+
+    pub fn read(&mut self, buf: &[u8], block_no: u32) -> i32 {
+        0
+    }
+
+    pub fn write(&mut self, buf: &[u8], block_no: u32) -> i32 {
+        0
+    }
+
+    pub fn init(&mut self) -> i32 {
+        0
     }
 }
