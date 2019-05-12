6add7002fd7b365f9b6cfe6ceda2855152b5b020
Yuhao Zhou <miskcoo@gmail.com>
2019-05-12 22:56:34 +0800

    Update consts for MIPS.malta

--- kernel/src/arch/mipsel/board/malta/consts.rs
+++ kernel/src/arch/mipsel/board/malta/consts.rs
@@ -1,3 +1,3 @@
 /// board specific constants
-pub const MEMORY_END: usize = 0x8080_0000;
-pub const KERNEL_HEAP_SIZE: usize = 0x0044_0000;
+pub const MEMORY_END: usize = 0x8090_0000;
+pub const KERNEL_HEAP_SIZE: usize = 0x003b_0000;
