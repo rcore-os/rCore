fbca9a6e3a57e13dc62d10824db9278ea403dcd5
Harry Chen <i@harrychen.xyz>
2019-05-14 00:21:04 +0800

    Add conditional compilation for mipsel, fix heap size for thinpad
    Signed-off-by: Harry Chen <i@harrychen.xyz>

--- kernel/src/arch/mipsel/driver/mod.rs
+++ kernel/src/arch/mipsel/driver/mod.rs
@@ -10,7 +10,7 @@ pub mod console;
 /// Initialize common drivers
 pub fn init() {
     board::init_driver();
-    // console::init();
+    console::init();
     if let Some(con) = console::CONSOLE.lock().as_mut() {
         con.clear();
     }
