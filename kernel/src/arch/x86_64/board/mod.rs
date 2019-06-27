4607e0646d9d0ba8033a0f0bcf77df8f2a13ebe2
Jiajie Chen <noc@jiegec.ac.cn>
2019-06-27 11:28:03 +0800

    Fix vga in vmware

--- kernel/src/arch/x86_64/board/mod.rs
+++ kernel/src/arch/x86_64/board/mod.rs
@@ -8,17 +8,35 @@ use core::mem::zeroed;
 use fb::{ColorConfig, FramebufferInfo, FramebufferResult, FRAME_BUFFER};
 
 static mut VBE_MODE: VbeModeInfo = VbeModeInfo {
-    _1: [0; 6],
+    attributes: 0,
+    win_a: 0,
+    win_b: 0,
+    granularity: 0,
     window_size: 0,
     segment_a: 0,
     segment_b: 0,
-    _2: 0,
+    _1: 0,
     pitch: 0,
     width: 0,
     height: 0,
-    _3: [0; 3],
+    w_char: 0,
+    y_char: 0,
+    planes: 0,
     bpp: 0,
-    _4: [0; 14],
+    banks: 0,
+    memory_model: 0,
+    bank_size: 0,
+    image_pages: 0,
+    _2: 0,
+    red_mask: 0,
+    red_position: 0,
+    green_mask: 0,
+    green_position: 0,
+    blue_mask: 0,
+    blue_position: 0,
+    rsv_mask: 0,
+    rsv_position: 0,
+    directcolor_attributes: 0,
     framebuffer: 0,
 };
 
@@ -48,10 +66,13 @@ pub fn probe_fb_info(_width: u32, _height: u32, _depth: u32) -> FramebufferResul
         bus_addr: framebuffer as u32,
         screen_size: width * height * (depth / 8),
     };
-    // assume BGRA8888 for now
-    Ok((
-        fb_info,
-        fb::ColorConfig::BGRA8888,
-        phys_to_virt(framebuffer),
-    ))
+    let config = if depth == 8 {
+        fb::ColorConfig::RGB332
+    } else if depth == 16 {
+        fb::ColorConfig::RGB565
+    } else {
+        // assume BGRA8888 for now
+        fb::ColorConfig::BGRA8888
+    };
+    Ok((fb_info, config, phys_to_virt(framebuffer)))
 }
